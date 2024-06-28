65fdecbb6790d664371813bdd0e05e90313be88a
David Liebman <radiodee1@gmail.com>
2024-06-28 08:14:35 -0400

    using python 3.10

---
+++ do_00_setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# pip install requirements-parser
+
+../flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.freedesktop.Sdk//23.08' --requirements-file='virtualenv/requirements.x86_64.txt' --output requirements.json
+
---
+++ virtualenv/do_make_virtualenv_setup310.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+python3.10 -m venv $HOME/.virtualenvs/pillm310 --prompt pillm310 
+
+. $HOME/.virtualenvs/pillm310/bin/activate
+
+#pip install -U pipeline-ai
+
+#pipeline cluster login chatbert311 $LLAMA_PIPELINE -u https://mystic.ai -a 
+
+
+## type `deactivate` to exit ##
