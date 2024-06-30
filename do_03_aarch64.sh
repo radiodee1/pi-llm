68dcb7119d9578c2f18dcdeec0f7e3e61bb99827
David Liebman <radiodee1@gmail.com>
2024-06-30 15:01:40 -0400

    make aarch64 package

---
+++ do_03_aarch64.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+flatpak install flathub org.gnome.Sdk/aarch64/43 -y 
+flatpak install flathub org.freedesktop.Platform/aarch64/23.08 -y
+flatpak install flathub org.freedesktop.Sdk/aarch64/23.08 -y 
+flatpak install flathub org.gnome.Platform/aarch64/43 -y
+
+flatpak-builder --user --arch=aarch64 --disable-tests --repo=repo build-dir org.llm.LLM.yaml --force-clean 
+
+#flatpak-builder --repo=repo --force-clean build-dir org.llm.LLM.yaml
+
+#flatpak-builder --install build-dir org.llm.LLM.yaml
+
+flatpak build-bundle --arch=aarch64 ./repo org.llm.LLM.aarch64.flatpak org.llm.LLM
