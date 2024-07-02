4f26c0e8fdd9256372e94024c8f5a80495a723f5
David Liebman <radiodee1@gmail.com>
2024-07-02 13:34:31 -0400

    trying to add second audio detection

--- src/llm.py
+++ src/llm.py
@@ -21,6 +21,11 @@ try:
 except:
     TEST_NINE = 16
 
+try:
+    MICROPHONE_INDEX=int(vals['MICROPHONE_INDEX'])
+except:
+    MICROPHONE_INDEX=-1
+
 test_txt = [ 
             'hi, my name is jane',
             'I like candy',
@@ -38,20 +43,22 @@ class Kernel:
         self.local = False
         self.remote = False
         self.test = False
+        self.x_iter = 1 ## start at 1
         self.q = mp.Queue()
 
     def loop(self):
         z = True
         x = 0 
         while z == True:
+            print("ai here")
             tt = test_txt[x]
             self.q = mp.Queue()
-            p = mp.Process(target=self.recognize_audio, args=(self.q,))
+            p = mp.Process(target=self.recognize_audio)
             p.start()
             rr = []
             time.sleep(2) 
             self.say_text(tt)
-            sleep_time = 0.75 * len(tt.split(" "))
+            sleep_time = 0.75 * len(tt.split(" ")) 
             print(sleep_time)
             time.sleep(sleep_time)
 
@@ -60,44 +67,72 @@ class Kernel:
                 print('rx', rx)
                 rr += [rx] 
             print(rr)
+            ## kill here ##
+            p.kill()
+            #p.close()
+            #p.kill()
             if self.is_match(tt.split(' '), rr):
-                print( 'no interruption' )
+                print( 'no interruption!' )
+                rr = []
+                self.q = mp.Queue()
             else:
                 print( 'interruption!' )
-            p.kill()
             x += 1
             x = x % len(test_txt)
-            g = input("type something ('stop' to quit) >> ")
-            if g == "stop":
-                z = False
-                p.kill()
-        print("here")
+            ### second process ###
+               
+            #time.sleep(5)
 
+            self.recognize_audio()
+            
+            while not self.q.empty():
+                rx = self.q.get()
+                print('rx2', rx)
+                rr += [rx]
+            
+            print("ai here")
 
-    def recognize_audio(self, q):
+    def list_microphones(self):
+        for k, v in enumerate(sr.Microphone.list_microphone_names()):
+            print(k, v)
+        print("---")
+        for k, v in enumerate(sr.Microphone.list_working_microphones()):
+            print(k,v)
+        print("---")
+        print(MICROPHONE_INDEX)
 
-        if True: # q.empty():
+    def recognize_audio(self):
 
-            r = sr.Recognizer()
-            with sr.Microphone() as source:
-                print("Say something!")
-                audio = r.listen(source)
+        #self.list_microphones()
+        r = sr.Recognizer()
+        #self.r.energy_threshold = 100
+        
+        with sr.Microphone() as source:
+            #r = sr.Recognizer()
+            print("Say something!")
+            audio = r.listen(source)
+            #audio = r.listen(source)
+            print("processing.")
 
-            try:
-                ret = r.recognize_google(audio)
-                # for testing purposes, we're just using the default API key
-                # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-                # instead of `r.recognize_google(audio)`
-                print("speech recognition: " + ret)
-                
-                for i in ret.split(' '):
-                    #print('sr', i)
-                    q.put(i)
-                
-            except sr.UnknownValueError:
-                print("Google Speech Recognition could not understand audio")
-            except sr.RequestError as e:
-                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        try:
+            ret = r.recognize_google(audio)
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            print("speech recognition: " + ret)
+            
+            for i in ret.split(' '):
+                print('sr', i)
+                self.q.put(i)
+            
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        
+        print('iter', self.x_iter)
+        self.x_iter += 1
+        return
 
     def say_text(self, txt):
         tts = gTTS(text=txt, lang='en')
