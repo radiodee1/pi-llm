fb59ebb9b08c9d890287e8c22e35128e949e963d
David Liebman <radiodee1@gmail.com>
2024-07-16 07:29:32 -0400

    change window to 35

--- src/llm.py
+++ src/llm.py
@@ -101,7 +101,7 @@ class Kernel:
         self.file_num = 0
         self.temp = 0.001
         self.timeout = 5.0
-        self.window = 40
+        self.window = 35 
         self.x_iter = 1 ## start at 1
         self.q = mp.Queue()
         self.prompt = ""
@@ -444,7 +444,7 @@ if __name__ == '__main__':
     parser.add_argument('--file', action="store_true", help="save statistics in text file.")
     parser.add_argument('--temp', type=float, default=0.001, help="temperature for LLM operation.")
     parser.add_argument('--timeout', type=float, default=5.0, help="minutes to timeout/quit.")
-    parser.add_argument('--window', type=int, default=40, help="number of memory units used in input.")
+    parser.add_argument('--window', type=int, default=35, help="number of memory units used in input.")
     ## NOTE: local is not implemented!! 
     
     args = parser.parse_args()
