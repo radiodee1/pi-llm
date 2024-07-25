021b7b8b8acb5a91db0863cf0e67afb13e98b747
David Liebman <radiodee1@gmail.com>
2024-07-25 14:03:09 -0400

    plot some points and looks good

--- script/plot.py
+++ script/plot.py
@@ -10,6 +10,8 @@ import nltk
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np 
+from sklearn.manifold import TSNE
+import numpy as np
 
 def preprocess_text(text):
     text = re.sub('[^a-zA-Zа-яА-Я1-9]+', ' ', text)
@@ -35,8 +37,8 @@ def tsne_plot_similar_words(title, labels, embedding_clusters, word_clusters, a,
     plt.figure(figsize=(16, 9))
     colors = cm.rainbow(np.linspace(0, 1, len(labels)))
     for label, embeddings, words, color in zip(labels, embedding_clusters, word_clusters, colors):
-        x = embeddings[:][0]
-        y = embeddings[:][1]
+        x = embeddings[:,0]
+        y = embeddings[:,1]
         plt.scatter(x, y, c=color, alpha=a, label=label)
         for i, word in enumerate(words):
             plt.annotate(word, alpha=0.5, xy=(x[i], y[i]), xytext=(5, 2),
@@ -52,8 +54,8 @@ def tsne_plot_similar_words(title, labels, embedding_clusters, word_clusters, a,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Pi LLM Output File Counter", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--file', default=str, help="File name and path.")
-    parser.add_argument('--p', default=int, help="perplexity.")
-    parser.add_argument('--topn', default=int, help="topn value.")
+    parser.add_argument('--p', default=15, help="perplexity value.")
+    parser.add_argument('--topn', default=30, help="topn value.")
     args = parser.parse_args()
  
     if args.file.strip() != "":
@@ -68,9 +70,14 @@ if __name__ == '__main__':
         print(keys)
 
     ## we're not using perplexity...
-    TOPN = 30 
+    TOPN = 30
+    PERPLEXITY = 15 
+
     if args.topn != None and int(args.topn) > 0:
         TOPN = int(args.topn)
+
+    if args.p != None and int(args.p) > 0:
+        PERPLEXITY = int(args.p)
 
     model = gensim.models.KeyedVectors.load_word2vec_format('../../GoogleNews-vectors-negative300.bin', binary=True)
 
@@ -86,9 +93,13 @@ if __name__ == '__main__':
         word_clusters.append(words)
 
 
-    embeddings_en_2d = embedding_clusters
+    #embeddings_en_2d = embedding_clusters
     #% matplotlib inline
 
+    embedding_clusters = np.array(embedding_clusters)
+    n, m, k = embedding_clusters.shape
+    tsne_model_en_2d = TSNE(perplexity=PERPLEXITY, n_components=2, init='pca', n_iter=3500, random_state=32)
+    embeddings_en_2d = np.array(tsne_model_en_2d.fit_transform(embedding_clusters.reshape(n * m, k))).reshape(n, m, 2)
 
     tsne_plot_similar_words('Similar words from Google News', keys, embeddings_en_2d, word_clusters, 0.7,
                         'similar_words.png')
