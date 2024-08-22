aef5f63a9c02dc6f1fc695b8904c83efc20c7cc5
David Liebman <radiodee1@gmail.com>
2024-08-22 08:36:41 -0400

    remove some comment lines

--- src/llm.py
+++ src/llm.py
@@ -310,40 +310,31 @@ class Kernel:
             tts.save(filename)
 
         if self.cloud_tts:
-            # Instantiates a client
             client = texttospeech.TextToSpeechClient()
 
-            # Set the text input to be synthesized
             synthesis_input = texttospeech.SynthesisInput(text=txt)
 
-            # Build the voice request, select the language code ("en-US") and the ssml
-            # voice gender ("neutral")
-            voice = texttospeech.VoiceSelectionParams( name=self.voice,
-                language_code="en-US" 
+            voice = texttospeech.VoiceSelectionParams( 
+                name=self.voice, language_code="en-US" 
             )
 
-            # Select the type of audio file you want returned
             audio_config = texttospeech.AudioConfig(
                 audio_encoding=texttospeech.AudioEncoding.MP3
             )
 
-            # Perform the text-to-speech request on the text input with the selected
-            # voice parameters and audio file type
             response = client.synthesize_speech(
                 input=synthesis_input, voice=voice, audio_config=audio_config
             )
 
-            # The response's audio_content is binary.
             with open(filename, "wb") as out:
                 # Write the response to the output file.
                 out.write(response.audio_content)
-                print('Audio content written to file ".output.mp3"')        
+                print('Audio content written to file "' + filename + '"')        
 
             pass 
 
         #m = hashlib.sha256()
         #f = m.hexdigest()
-        filename =  '.output.mp3'
         #tts.save(filename)
         playsound(filename)
         self.p('say this: ', txt)
