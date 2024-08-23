b82926bbfe7f77139ef693de7ad08a134474e18c
David Liebman <radiodee1@gmail.com>
2024-08-23 10:23:34 -0400

    default half a minute to timeout

--- src/llm.py
+++ src/llm.py
@@ -531,7 +531,7 @@ if __name__ == '__main__':
     parser.add_argument('--mics', action="store_true", help="display microphone data and quit.")
     parser.add_argument('--file', action="store_true", help="save statistics in text file.")
     parser.add_argument('--temp', type=float, default=0.001, help="temperature for LLM operation.")
-    parser.add_argument('--timeout', type=float, default=3.0, help="minutes to timeout.")
+    parser.add_argument('--timeout', type=float, default=0.5, help="minutes to timeout.")
     parser.add_argument('--window', type=int, default=35, help="number of memory units used in input.")
     parser.add_argument('--cloud_stt', action="store_true", help="Google Cloud Speech Recognition.")
     parser.add_argument('--cloud_tts', action="store_true", help="Google Cloud Text to Speech.")
