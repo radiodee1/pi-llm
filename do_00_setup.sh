500280678a36d46e2e3ecc6bbf0af1dc4ff0e0a8
David Liebman <radiodee1@gmail.com>
2024-08-26 17:03:12 -0400

    no change

--- do_00_setup.sh
+++ do_00_setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 
-../flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' --requirements-file='virtualenv/requirements.x86_64.txt' --ignore-installed=setuptools,portaudio,PyAudio,pygobject --ignore-errors --output requirements
+../flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' --requirements-file='virtualenv/requirements.x86_64.txt' --ignore-installed=setuptools,portaudio,PyAudio,pygobject --ignore-errors --output requirements ## --runtime='org.gnome.Sdk//45'
 
