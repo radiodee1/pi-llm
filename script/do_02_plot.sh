32e18bdf4608bf73a69e698ac025e2d4148d1bf2
David Liebman <radiodee1@gmail.com>
2024-09-22 12:47:40 -0400

    plot script

--- script/do_02_plot.sh
+++ script/do_02_plot.sh
@@ -1,11 +1,28 @@
 #!/bin/bash 
 
-echo "plot the points from the 'count.py' stage."
-echo "this uses the GoogleNews data."
+PLOTINPUT="../txt/llm.dialogue.count.txt"
 
+if [ $# -eq '1' ]; then
+    PLOTINPUT=$1
+fi 
+
+echo ""
+echo "the input file is specified as the first"
+echo "argument on the command line."
+
+echo ""
+
+echo "the input file is currently set to " $PLOTINPUT
+echo ""
+
+echo "use this script to plot the points from the "
+echo "'count.py' stage."
+echo ""
+echo "this uses the GoogleNews data and takes some time."
+echo ""
 read  -n 1 -p "Continue/Break(Ctl-c):" xinput
 
 echo $xinput
 
-./plot.py --file ../txt/llm.dialogue.count.txt --bin ../../GoogleNews-vectors-negative300.bin --output ../png/llm.dialogue
+./plot.py --file $PLOTINPUT --bin ../../GoogleNews-vectors-negative300.bin --output ../png/llm.dialogue
 
