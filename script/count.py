f54c9045c516cc8ba81ac61af2562392c7e4db51
David Liebman <radiodee1@gmail.com>
2024-10-11 11:46:11 -0400

    working on count.py - not finished

--- script/count.py
+++ script/count.py
@@ -3,7 +3,7 @@
 import argparse
 
 blacklist = ['and', 'to', 'the', 'for', 'a', 'is', 'of', 'on', '', ')', '(']
-HIGH_LIMIT = 0 
+
 
 bad_characters = [ ',', '!', '.', '?', "'", "’" ]
 
@@ -20,10 +20,11 @@ class Kernel:
         self.count = -1 
         self.combine = False
         self.save = False
+        self.high_limit = 0
 
     def open_and_count(self):
         if self.file.strip() != "":
-            self.file_list.append(self.file.strip())
+            #self.file_list.append(self.file.strip())
             f = open(self.file, 'r')
             x = f.readlines()
             f.close()
@@ -44,31 +45,36 @@ class Kernel:
         pass 
 
     def print_stats(self):
+        print(self.dict_words)
         high = 0
         key_record = ''
         count = 0
+        has_written = False
         total = len(self.dict_words)
         for _ in range(len(self.dict_words)):
             for key in self.dict_words:
                 if self.dict_words[key] > high:
                     key_record = key
                     high = self.dict_words[key]
-            if key_record not in blacklist and self.dict_words[key_record] > HIGH_LIMIT:
+            if key_record not in blacklist and self.dict_words[key_record] > self.high_limit :
                 print(key_record + ',', self.dict_words[key_record])
                 self.file_output(key_record + ", " + str(self.dict_words[key_record]))
+                has_written = True 
                 if count >= self.count and self.count != -1:
                     break 
                 count += 1
             del self.dict_words[key_record]
             high = 0
             key_record = ''
+        if not has_written:
+            self.file_output("")
         print()
         print('displayed:' , count, 'categories:', total, 'words:', self.num_words ,'sentence:', self.num_words / float(self.num_sentences))
         print('sentences:', self.num_sentences, 'exchanges:', self.num_sentences / 2, end=" ")
         print('restarts:', self.num_restarts, 'files:', self.file_list)
         pass 
 
-    def file_output(self, line_out):
+    def file_output(self, line_out=""):
         if not self.save:
             return
 
@@ -84,7 +90,7 @@ class Kernel:
             #name = name[:-4]
 
         #name += '.count.txt'
-        #print("Name output:", name)
+        print("Name output:", name)
         f = open(name, 'a')
         f.write(line_out + "\n")
         f.close()
@@ -102,20 +108,25 @@ if __name__ == '__main__':
     
     print(args)
     
-    if args.combine != None:
+    def setup_args():
+        k = Kernel()
+
         k.combine = args.combine
 
-    if args.low != None and int(args.low) >= -1:
-        HIGH_LIMIT = int(args.low)
+        if args.low != None and int(args.low) >= -1:
+            k.high_limit = int(args.low)
 
-    if args.count != None and int(args.count) != -1:
-        k.count = int(args.count) - 1 
+        if args.count != None and int(args.count) != -1:
+            k.count = int(args.count) - 1 
 
-    if args.save != None:
-        k.save = args.save
+        if args.save != None:
+            k.save = args.save
 
-    if args.files != None and len(args.files) > 0:
-        if k.combine:
+        print("setupt", args)
+
+    if args.files != None and args.combine and len(args.files) > 0:
+        if args.combine:
+            setup_args()
             for i in args.files:
                 k.file = i
                 if k.file.endswith(".count.txt"):
@@ -126,7 +137,9 @@ if __name__ == '__main__':
 
     if args.files != None and not args.combine and len(args.files) > 0:
         for i in args.files:
-            k.file = i 
+            setup_args()
+            k.file = i
+            print(k.file, 'i file')
             if k.file.endswith(".count.txt"):
                 continue
             k.open_and_count()
