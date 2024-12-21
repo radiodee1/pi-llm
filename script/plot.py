02de017f7acf52fffbdae5d1e87e87ce4f24c5af
David Liebman <radiodee1@gmail.com>
2024-12-21 11:44:13 -0500

    try using tsne to measure distance from origin

--- script/plot.py
+++ script/plot.py
@@ -10,6 +10,7 @@ import nltk
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np 
+from numpy.linalg import cond
 from sklearn.manifold import TSNE
 import numpy as np
 import math 
@@ -47,12 +48,16 @@ if __name__ == '__main__':
     parser.add_argument('--topn', default=5, help="topn value.")
     parser.add_argument('--all', action="store_true", help="plot all words (include test words.)")
     parser.add_argument('--single_graph', action="store_true", help="plot all words on single graph.")
+    parser.add_argument('--words', default=-1, help="number of words from each count document.")
     args = parser.parse_args()
     
     embedding_clusters = []
     word_clusters = []
     filename_clusters = []
 
+    PERPLEXITY = 15 
+    out_path = "." 
+    OUTPUT_FILE = '/diagram.png'
 
     if len(args.files) > 1:
         show_plot = False
@@ -80,8 +85,8 @@ if __name__ == '__main__':
             if args.topn != None and int(args.topn) > 0:
                 TOPN = int(args.topn)
 
-            if args.p != None and int(args.p) > 0:
-                PERPLEXITY = int(args.p)
+            if args.p != None and float(args.p) >= 0:
+                PERPLEXITY = float(args.p)
 
             if args.bin != None and str(args.bin).strip() != "":
                 W2V_BIN = str(args.bin).strip()
@@ -105,6 +110,9 @@ if __name__ == '__main__':
                 embeddings = []
                 words = []
                 filenames = []
+                #if int(args.words) != -1 and num >= int(args.words):
+                #    print(num, 'break')
+                #    break
                 if word not in model:
                     print('not in model', word)
                     continue
@@ -123,36 +131,47 @@ if __name__ == '__main__':
                 filename_clusters.append(filenames)
 
 
-            #embeddings_en_2d = embedding_clusters
-            #% matplotlib inline
+    embedding_clusters = np.array(embedding_clusters)
+    n, m, k = embedding_clusters.shape
+    tsne_model_en_2d = TSNE(perplexity=PERPLEXITY, n_components=2, init='pca', n_iter=3500, random_state=32)
+    embeddings_en_2d = np.array(tsne_model_en_2d.fit_transform(embedding_clusters.reshape(n * m, k))).reshape(n, m, 2)
 
-            embedding_clusters = np.array(embedding_clusters)
-            n, m, k = embedding_clusters.shape
-            tsne_model_en_2d = TSNE(perplexity=PERPLEXITY, n_components=2, init='pca', n_iter=3500, random_state=32)
-            embeddings_en_2d = np.array(tsne_model_en_2d.fit_transform(embedding_clusters.reshape(n * m, k))).reshape(n, m, 2)
+    if not args.single_graph:
+        tsne_plot_similar_words('Similar words from Google News', keys, embeddings_en_2d, word_clusters, 0.7,
+                                OUTPUT_FILE)
+        print('keys', len(keys))
+        #exit()
 
-            tsne_plot_similar_words('Similar words from Google News', keys, embeddings_en_2d, word_clusters, 0.7,
-                                    OUTPUT_FILE)
-            print('keys', len(keys))
-            #print(embeddings_en_2d)
-
-            oldf = ""
+    print(embeddings_en_2d) 
+    ff = open(out_path + "/distance.csv", 'w')
+    oldf = ""
+    dist = 0
+    xold = 0 
+    yold = 0
+    num = 0 
+    for embeddings, file in zip(embeddings_en_2d, filename_clusters):
+        x = embeddings[:,0][0]
+        y = embeddings[:,1][0]
+        f = file[0]
+        if oldf == f:
+            #print('here...')
+            tempx = x - xold
+            tempy = y - yold
+            tempx = tempx * tempx
+            tempy = tempy * tempy
+            dist = math.sqrt(tempx + tempy )
+        else:
             dist = 0
-            xold = 0 
-            yold = 0 
-            for embeddings, file in zip(embeddings_en_2d, filename_clusters):
-                x = embeddings[:,0][0]
-                y = embeddings[:,1][0]
-                f = file[0]
-                if oldf == f:
-                    #print('here...')
-                    tempx = x - xold
-                    tempy = y - yold
-                    tempx = tempx * tempx
-                    tempy = tempy * tempy
-                    dist = math.sqrt(tempx + tempy )
-                oldf = f 
-                xold = x 
-                yold = y 
-                print(x,y, f, dist)
-                pass
+            num = 0
+            print(f)
+        dist_origin = math.sqrt(x * x + y * y)
+        oldf = f 
+        xold = x 
+        yold = y 
+        if num >= int(args.words):
+            continue 
+        print(num, x,y, f, dist, dist_origin)
+        ff.write(str(x) + ", " + str(y) + ", " + str(f) + ", " + str(dist) + ", " + str(dist_origin) + "\n")
+        num += 1 
+        pass
+    ff.close()
