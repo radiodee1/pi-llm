3a88e98b0338f5dce9a51131c7c4ef401c5b717c
David Liebman <radiodee1@gmail.com>
2025-03-27 14:05:12 -0400

    change to gnome 48 and pygobject 3.52

--- do_00_setup.sh
+++ do_00_setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+flatpak install flathub org.gnome.Sdk//48 -y 
+flatpak install flathub org.freedesktop.Platform//24.08 -y
+flatpak install flathub org.freedesktop.Sdk//24.08 -y 
+flatpak install flathub org.gnome.Platform//48 -y
 
-../flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//45' --requirements-file='virtualenv/requirements.flatpak.txt' --ignore-installed=setuptools,portaudio,PyAudio,pygobject --ignore-errors --output requirements ## --runtime='org.gnome.Sdk//45'
+
+../flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//48' --requirements-file='virtualenv/requirements.flatpak.txt' --ignore-installed=setuptools,portaudio,PyAudio,pygobject --ignore-errors --output requirements ## --runtime='org.gnome.Sdk//45'
 
--- do_01_build.sh
+++ do_01_build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-flatpak install flathub org.gnome.Sdk//45 -y 
-flatpak install flathub org.freedesktop.Platform//23.08 -y
-flatpak install flathub org.freedesktop.Sdk//23.08 -y 
-flatpak install flathub org.gnome.Platform//45 -y
+flatpak install flathub org.gnome.Sdk//48 -y 
+flatpak install flathub org.freedesktop.Platform//24.08 -y
+flatpak install flathub org.freedesktop.Sdk//24.08 -y 
+flatpak install flathub org.gnome.Platform//48 -y
 
 flatpak-builder --user --install build-dir org.llm.LLM.yaml --force-clean 
 
