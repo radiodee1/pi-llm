d816cc53578c194d8f85b3d2a03a53622127fd06
David Liebman <radiodee1@gmail.com>
2025-03-27 16:13:55 -0400

    build aarch64

--- virtualenv/do_install_apt_pkg.sh
+++ virtualenv/do_install_apt_pkg.sh
@@ -1,2 +1,2 @@
-sudo apt install python3-dotenv python3-pyaudio portaudio19-dev python3-espeak python3-mesonpy libcairo2-dev libgirepository1.0-dev libsqlite3-dev libghc-setenv-dev -y
+sudo apt install python3-dotenv python3-pyaudio portaudio19-dev python3-espeak python3-mesonpy libcairo2-dev libgirepository1.0-dev libsqlite3-dev libghc-setenv-dev  gcc make gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu qemu-system-arm qemu-user-static -y
 
