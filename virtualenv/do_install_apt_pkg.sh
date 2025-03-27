c5a00d2f7c3c772b3a505c675b41b705b27c7106
David Liebman <radiodee1@gmail.com>
2025-03-27 16:18:56 -0400

    note in install file

--- virtualenv/do_install_apt_pkg.sh
+++ virtualenv/do_install_apt_pkg.sh
@@ -1,2 +1,5 @@
 sudo apt install python3-dotenv python3-pyaudio portaudio19-dev python3-espeak python3-mesonpy libcairo2-dev libgirepository1.0-dev libsqlite3-dev libghc-setenv-dev  gcc make gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu qemu-system-arm qemu-user-static -y
 
+echo ""
+echo "NOTE: you may need to start qemu-system-arm to enable cross compiling."
+echo "$ sudo systemctl restart systemd-binfmt.service"
