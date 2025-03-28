09883cadf87e048377c21a2813bb0f9a97c92cb7
David Liebman <radiodee1@gmail.com>
2025-03-28 13:32:41 -0400

    prep

---
+++ do_05_prep_flatpak.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+flatpak install flathub org.gnome.Sdk//48 -y --user
+flatpak install flathub org.freedesktop.Platform//24.08 -y --user 
+flatpak install flathub org.freedesktop.Sdk//24.08 -y --user 
+flatpak install flathub org.gnome.Platform//48 -y --user
+
+
+
