ef1ddf7990ca30f6e1de4741d9fb09aa804bf23f
David Liebman <radiodee1@gmail.com>
2025-03-28 08:38:03 -0400

    comments

--- virtualenv/do_make_virtualenv_setup310.sh
+++ virtualenv/do_make_virtualenv_setup310.sh
@@ -4,9 +4,4 @@ python3.10 -m venv $HOME/.virtualenvs/pillm310 --prompt pillm310
 
 . $HOME/.virtualenvs/pillm310/bin/activate
 
-#pip install -U pipeline-ai
-
-#pipeline cluster login chatbert311 $LLAMA_PIPELINE -u https://mystic.ai -a 
-
-
 ## type `deactivate` to exit ##
