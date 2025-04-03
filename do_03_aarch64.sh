4c21693861d17dcc3d0e56ad58f3e7087842b6cc
David Liebman <radiodee1@gmail.com>
2025-04-03 15:32:07 -0400

    add --user flag

--- do_03_aarch64.sh
+++ do_03_aarch64.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-flatpak install flathub org.gnome.Sdk/aarch64/48 -y 
-flatpak install flathub org.freedesktop.Platform/aarch64/24.08 -y
-flatpak install flathub org.freedesktop.Sdk/aarch64/24.08 -y 
-flatpak install flathub org.gnome.Platform/aarch64/48 -y
+flatpak install flathub org.gnome.Sdk/aarch64/48 --user -y 
+flatpak install flathub org.freedesktop.Platform/aarch64/24.08 --user -y
+flatpak install flathub org.freedesktop.Sdk/aarch64/24.08 --user -y 
+flatpak install flathub org.gnome.Platform/aarch64/48 --user -y
 
 flatpak-builder --user --arch=aarch64 --disable-tests --repo=repo build-dir org.llm.LLM.yaml --force-clean 
 
