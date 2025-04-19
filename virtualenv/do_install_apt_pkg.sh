d18efce2fc5aa1892b1286c2167e90a697566a4c
David Liebman <radiodee1@gmail.com>
2025-04-19 11:32:44 -0400

    apt pkgs

--- virtualenv/do_install_apt_pkg.sh
+++ virtualenv/do_install_apt_pkg.sh
@@ -1,4 +1,4 @@
-sudo apt install python3-dotenv python3-pyaudio portaudio19-dev python3-espeak python3-mesonpy libcairo2-dev libgirepository1.0-dev libsqlite3-dev libghc-setenv-dev  gcc make gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu qemu-system-arm qemu-user-static -y
+sudo apt install python3-dotenv python3-pyaudio portaudio19-dev python3-espeak python3-mesonpy libcairo2-dev libgirepository1.0-dev libsqlite3-dev libghc-setenv-dev  gcc make gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu qemu-system-arm qemu-user-static libssl-dev tcl-dev tk-dev libgdbm-dev libgdbm-compat-dev lib32readline-dev libnsl-dev -y
 
 echo ""
 echo "NOTE: you may need to start qemu-system-arm to enable cross compiling."
