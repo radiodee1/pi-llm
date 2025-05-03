4273bb0f041770a177974d7540a6fd7a7d04a20a
David Liebman <radiodee1@gmail.com>
2025-05-03 12:29:33 -0400

    env vars for docker later

---
+++ env_docker/setup_user_dir.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+cd ../env 
+
+HOST_FILE='docker_volume.env'
+
+USER_DIR=''
+
+
+if [ $# -ne '1' ]; then
+    echo ""
+    echo "Enter a USER_DIR or leave blank for this user."
+    echo "Enter empty double quotation marks for 'none'."
+else
+    USER_DIR=$1 
+fi 
+
+
+echo "# env" > $HOST_FILE
+echo "ENV_USER_DIR=${USER_DIR}" >> $HOST_FILE
+echo "# empty?? " >> $HOST_FILE
+
---
+++ env_docker/setup_volume.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+cd ../env 
+
+HOST_FILE='docker_volume.env'
+
+VOLUME=''
+
+
+if [ $# -ne '1' ]; then
+    echo ""
+    echo "Enter a USER_DIR or leave blank for this user."
+    echo "Enter empty double quotation marks for 'none'."
+else
+    VOLUME=$1 
+fi 
+
+
+echo "# env" > $HOST_FILE
+echo "ENV_VOLUME=${VOLUME}" >> $HOST_FILE
+echo "# empty?? " >> $HOST_FILE
+
