2d227d428e34371d827795604f5c312590297f77
David Liebman <radiodee1@gmail.com>
2025-05-12 13:52:27 -0400

    pulse

--- do_07_docker_compose_win.sh
+++ do_07_docker_compose_win.sh
@@ -39,7 +39,9 @@ cp files/pulseaudio.client.conf src/.
 cp files/pulseaudio.default.pa src/. 
 cp files/pulseaudio.daemon.conf src/.
 
+echo "module-waveout"
 pactl load-module module-waveout sink_name=output source_name=input record=0
+echo "module-native-protocol-tcp"
 pactl load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1
 
 sudo ENV_USER_DIR=$USER_DIR ENV_UID=$UID ENV_GID=$GROUP ENV_PWD=$USER_PWD docker compose --env-file ./env_docker/docker_volume.env -f compose-win.yaml up   
