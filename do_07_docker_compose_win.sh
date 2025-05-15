60114855418a486a793eeaca4e9a1a951e4480b0
David Liebman <radiodee1@gmail.com>
2025-05-15 07:36:39 -0400

    pulse

--- do_07_docker_compose_win.sh
+++ do_07_docker_compose_win.sh
@@ -72,7 +72,7 @@ sudo cp files/*.daemon.conf /etc/pulse/daemon.conf
 #sudo chown -R 0 $USER_DIR
 chmod -R 777 $USER_DIR
 
-ENV_VOLUME=$USER_DIR ENV_USER_DIR=$USER_DIR ENV_UID=$UID ENV_GID=$GROUP  ENV_IP=$Hostip ENV_PULSE_SERVER=unix:/mnt/wslg/PulseServer docker compose -f compose-win.yaml --env-file ./pulseaudio-win.env up   
+sudo ENV_VOLUME=$USER_DIR ENV_USER_DIR=$USER_DIR ENV_UID=$UID ENV_GID=$GROUP  ENV_IP=$Hostip ENV_PULSE_SERVER=unix:/mnt/wslg/PulseServer docker compose -f compose-win.yaml --env-file ./pulseaudio-win.env up   
 
 
 
