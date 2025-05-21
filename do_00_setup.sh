d793c708ef08e6321703f6f19bdd49199d70de62
David Liebman <radiodee1@gmail.com>
2025-05-21 11:32:58 -0400

    broken -- does not build since virtualenv upgrade of setuptools

--- do_00_setup.sh
+++ do_00_setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
-flatpak install flathub org.gnome.Sdk//48 -y 
-flatpak install flathub org.freedesktop.Platform//24.08 -y
-flatpak install flathub org.freedesktop.Sdk//24.08 -y 
-flatpak install flathub org.gnome.Platform//48 -y
+flatpak install flathub org.gnome.Sdk//48 -y --user 
+flatpak install flathub org.freedesktop.Platform//24.08 -y --user 
+flatpak install flathub org.freedesktop.Sdk//24.08 -y --user 
+flatpak install flathub org.gnome.Platform//48 -y --user 
 
 
-../flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//48' --requirements-file='virtualenv/requirements.flatpak.txt' --ignore-installed=setuptools,portaudio,PyAudio,pygobject --ignore-errors --output requirements ## --runtime='org.gnome.Sdk//45'
+../flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//48' --requirements-file='virtualenv/requirements.flatpak.txt' --ignore-installed=setuptools,portaudio,PyAudio,pygobject  --output requirements ## --runtime='org.gnome.Sdk//45'
+
+#../flatpak-builder-tools/pip/flatpak-pip-generator --runtime='org.gnome.Sdk//48' --requirements-file='virtualenv/requirements.flatpak.txt' --ignore-installed=setuptools,portaudio,PyAudio,pygobject --ignore-errors --output requirements ## --runtime='org.gnome.Sdk//45'
 
